63a42d6aaf47bdaf5114dc42a7fe0a03ed96071c
Shih-Yi <brvast@gmail.com>
2025-05-12 17:45:53 +1200

    fix: update i18n settings

--- config/i18n.ts
+++ config/i18n.ts
@@ -6,9 +6,11 @@ import { join } from 'node:path'
  * 根據環境決定語言文件路徑
  */
 function getLangFilesPath() {
-  const path = app.inProduction ? join(process.cwd(), 'resources/lang') : app.languageFilesPath()
+  const path = app.inProduction
+    ? join(process.cwd(), 'current/resources/lang')
+    : app.languageFilesPath()
 
-  console.log(`${app.inProduction} -[i18n] Using language path: ${path}`)
+  console.log(`[i18n] Using language path: ${path}`)
   console.log(`[i18n] App in production: ${app.inProduction}`)
 
   return path
